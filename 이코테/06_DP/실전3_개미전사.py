2e897720cad7f76b9f5fc06e711785e9f7ce5c08
Jinseo <2000jinseo@gmail.com>
2023-08-02 23:11:40 +0900

    230802 이코테 DP (실전-개미전사)

---
+++ 이코테/06_DP/실전3_개미전사.py
@@ -0,0 +1,14 @@
+# DP
+# 개미 전사
+
+n = int(input())
+k = list(map(int, input().split()))
+
+d = [0] * 1001
+d[0] = k[0]
+d[1] = max(k[0], k[1])
+
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2]+k[i])
+
+print(d[n-1])
