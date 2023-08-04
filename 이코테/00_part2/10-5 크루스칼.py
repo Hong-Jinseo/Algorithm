235ea88818ccb77a165b5f53a554b46403d54253
Jinseo <2000jinseo@gmail.com>
2023-08-04 12:10:41 +0900

    230804 이코테 그래프이론 개념

---
+++ 이코테/00_part2/10-5 크루스칼.py
@@ -0,0 +1,60 @@
+# 크루스칼 알고리즘 소스코드
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선(union 연산)의 개수
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+for i in range(1, v+1):
+    parent[i] = i
+
+edges = []
+result = 0
+
+# 간선 정보 입력받기
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
+
+'''input
+7 9
+1 2 29
+1 5 75
+2 3 35
+2 6 34
+3 4 7
+4 6 23
+4 7 13
+5 6 53
+6 7 25
+'''
+
+'''output
+159
+'''
