555d2473619599474388d1843182a343a05a7e9b
Jinseo <2000jinseo@gmail.com>
2023-08-16 02:01:38 +0900

    230815 브루트 포스 (카잉 달력) #2

--- baekjoon/6064.py
+++ baekjoon/6064.py
@@ -2,16 +2,18 @@
 # 카잉 달력
 
 t = int(input())
+answer = []
 
 for _ in range(t):
     m, n, x, y = map(int, input().split())
+
     found = False
     while x <= m * n:
-        if x % n == y % n:
+        if abs(x - y) % n == 0:
             print(x)
             found = True
             break
-        x += m
+        x += m      # m을 더해도 카잉달력 기준으로 x값은 동일함
 
     if not found:
         print(-1)
