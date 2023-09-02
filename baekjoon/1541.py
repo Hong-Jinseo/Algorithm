4cf493d12ff6ce2bd6a8e55f120ccf67bd980f0e
Jinseo <2000jinseo@gmail.com>
2023-09-03 01:01:01 +0900

    230902 그리디 (잃어버린 괄호) #2

--- baekjoon/1541.py
+++ baekjoon/1541.py
@@ -1,12 +1,18 @@
 # 그리디
 # 잃어버린 괄호
 
+# 덧셈 할 숫자들 묶기
 exp = input().split('-')
-ans = 0
-for i in exp[0].split('+'):
-    ans += int(i)
-for i in exp[1:]:
-    for j in i.split('+'):
-        ans -= int(j)
 
-print(ans)
+sub = []
+for e in exp:
+    # 덧셈 수행
+    temp = e.split('+')
+    sub.append(sum(map(int, temp)))
+
+# 뺄셈 수행
+answer = sub[0]
+for s in sub[1:]:
+    answer -= s
+
+print(answer)
