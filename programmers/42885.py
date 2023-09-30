975c8f1da44c5e546699217b9d461cab83de8c8c
Jinseo <2000jinseo@gmail.com>
2023-09-30 17:46:55 +0900

    230930 그리디 (구명보트) #2

--- programmers/42885.py
+++ programmers/42885.py
@@ -1,19 +1,21 @@
 # 그리디
 # 구명보트
 
+# 2차 풀이 (전과 동일)
 def solution(people, limit):
-    n = len(people)
+    pair = 0
+    h, t = 0, len(people) - 1  # 머리, 꼬리
     people.sort()
 
-    i, j = 0, n - 1
-    pair = 0
-
-    while i < j:
-        if people[i] + people[j] <= limit:
-            i += 1
-            j -= 1
+    while h < t:
+        # 짝이 지어지면
+        if people[h] + people[t] <= limit:
             pair += 1
-        else:
-            j -= 1
+            h += 1
+            t -= 1
 
-    return n - pair
+        # 짝이 지어지지 않으면 -> 무거운 사람 혼자 보트에 탑승
+        else:
+            t -= 1
+
+    return len(people) - pair
