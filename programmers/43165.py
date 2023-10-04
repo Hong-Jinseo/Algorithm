635b91e5796401ba0be231aa2fa0a6c0892c45bb
Jinseo <2000jinseo@gmail.com>
2023-10-05 01:53:59 +0900

    231005 DFS (타겟 넘버) #2

--- programmers/43165.py
+++ programmers/43165.py
@@ -1,6 +1,22 @@
 # DFS 깊이 우선 탐색
 # 타겟 넘버
 
+def dfs(nums, idx, total, target, cnt):
+    if idx == len(nums):
+        return cnt + 1 if total == target else cnt
+
+    cnt = dfs(nums, idx + 1, total + nums[idx], target, cnt)
+    cnt = dfs(nums, idx + 1, total - nums[idx], target, cnt)
+
+    return cnt
+
+
+def solution(numbers, target):
+    answer = dfs(numbers, 0, 0, target, 0)
+    return answer
+
+
+'''
 # dfs(숫자 배열, 현재까지 누적 값, 인덱스, 타겟값, 타겟을 달성한 횟수)
 def dfs(arr, now, index, target, count):
     # 마지막 숫자까지 처리했다면
@@ -18,3 +34,4 @@ def solution(numbers, target):
     print(answer)
 
     return answer
+'''
