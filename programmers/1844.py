bdebb81ad85d91f07f39c0175b8d7668f6bfacda
Jinseo <2000jinseo@gmail.com>
2023-10-05 02:10:06 +0900

    231005 BFS (게임 맵 최단거리) #2

--- programmers/1844.py
+++ programmers/1844.py
@@ -5,30 +5,29 @@ from collections import deque
 
 
 def solution(maps):
-    dx = [0, 0, 1, -1]
+    n = len(maps)  # x
+    m = len(maps[0])  # y
+
+    dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
+    visited = [[False] * m for _ in range(n)]
 
-    n = len(maps)
-    m = len(maps[0])
-
-    q = deque([(0, 0, 1)])  # (x, y, move)
-    visited = [[0] * m for _ in range(n)]
+    q = deque([(0, 0, 1)])  # (x좌표, y좌표, 움직인 횟수)
     visited[0][0] = True
 
     while q:
         x, y, move = q.popleft()
 
+        if x == n - 1 and y == m - 1:
+            return move
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m:
-                # 다음 노드가 상대 진영이면
-                if nx == n - 1 and ny == m - 1:
-                    return move + 1
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                if maps[nx][ny] == 1:  # 벽이 없으면
+                    q.append((nx, ny, move + 1))
+                    visited[nx][ny] = True
 
-                # 다음 노드가 1이면
-                elif maps[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny, move + 1))  # 방문 예정 목록에 추가
-                    visited[nx][ny] = True  # 방문 기록
     return -1
