35dbd8aeaed7b81038f338423be7c690c68ef28f
Jinseo <2000jinseo@gmail.com>
2023-10-16 20:52:12 +0900

    231016 BFS (단어 변환) #2

--- programmers/43163.py
+++ programmers/43163.py
@@ -1,6 +1,45 @@
 # BFS
 # 단어 변환
 
+# 2차 풀이
+from collections import deque
+
+
+def can_change(begin, target):
+    cnt = 0
+    for b, t in zip(begin, target):
+        # 같은 순서의 알파벳이 서로 다르다면 cnt += 1
+        if b != t:
+            cnt += 1
+
+    # 서로 다른 알파벳이 하나뿐이라면 True 반환
+    return True if cnt == 1 else False
+
+
+def solution(begin, target, words):
+    n = len(words)
+
+    used = [False] * n
+    q = deque([(begin, 0)])
+
+    while q:
+        now, cnt = q.popleft()
+
+        for i, word in enumerate(words):
+            # 아직 사용하지 않았고 and 바꿀 수 있는 단어라면
+            if not used[i] and can_change(now, word):
+
+                # 그 단어가 타겟 단어라면
+                if word == target:
+                    return cnt + 1
+
+                used[i] = True
+                q.append((word, cnt + 1))
+
+    return 0
+
+'''
+# 1차 풀이
 from collections import deque
 
 
@@ -33,6 +72,6 @@ def solution(begin, target, words):
                 visited.append(post)
 
     return 0
-
+'''
 
 print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
