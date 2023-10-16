5c10f2419148c02261d276a2c09dd46fcc325909
Jinseo <2000jinseo@gmail.com>
2023-10-16 20:20:57 +0900

    231016 DFS (네트워크) #2

--- programmers/43162.py
+++ programmers/43162.py
@@ -1,6 +1,34 @@
 # DFS 깊이 우선 탐색
 # 네트워크
 
+# 2차 풀이
+def dfs(num, computers, visited):
+    for idx, connected in enumerate(computers[num]):
+        # 연결되어있지만 아직 방문하지 않았다면
+        if connected and not visited[idx]:
+            # 방문 기록하기
+            visited[idx] = True
+            # 깊이우선탐색
+            dfs(idx, computers, visited)
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+
+    for i in range(n):
+        # 이미 방문한 컴퓨터라면
+        if visited[i]:
+            continue
+
+        # 첫 방문이라면
+        answer += 1
+        dfs(i, computers, visited)
+
+    return answer
+
+'''
+# 1차 풀이
 def dfs(j, computers, visited):
     visited[j] = True
     for k in range(len(computers[0])):
@@ -21,7 +49,7 @@ def solution(n, computers):
 
     print(answer)
     return answer
-
+'''
 
 solution(4, [[1, 1, 0, 1], [1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 1]])
 # 1
