4d80ff4d103c186e1f368fcbe707fc5c1373890c
Jinseo <2000jinseo@gmail.com>
2023-10-17 13:59:14 +0900

    231017 BFS (아이템 줍기) #2

--- programmers/87694.py
+++ programmers/87694.py
@@ -4,6 +4,67 @@
 from collections import deque
 
 
+# 2차 풀이
+def set_status(status, rectangle):
+    for lx, ly, rx, ry in rectangle:
+        # 테두리(세로)
+        for i in range(ly, ry + 1):
+            status[lx][i] = 1 if status[lx][i] != 0 else 0
+            status[rx][i] = 1 if status[rx][i] != 0 else 0
+
+        # 테두리(가로)
+        for i in range(lx, rx + 1):
+            status[i][ly] = 1 if status[i][ly] != 0 else 0
+            status[i][ry] = 1 if status[i][ry] != 0 else 0
+
+        # 내부
+        for i in range(lx + 1, rx):
+            for j in range(ly + 1, ry):
+                status[i][j] = 0
+
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    MAX = max(max(r) for r in rectangle) * 2 + 1
+
+    # 값 2배로 늘리기
+    for i in range(len(rectangle)):
+        for j in range(4):
+            rectangle[i][j] *= 2
+    characterX *= 2
+    characterY *= 2
+    itemX *= 2
+    itemY *= 2
+
+    # -1 외부, 0 내부, 1 테두리
+    status = [[-1] * MAX for _ in range(MAX)]
+    set_status(status, rectangle)
+
+    # BFS
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    q = deque([(characterX, characterY, 0)])  # x, y, move
+
+    while q:
+        x, y, move = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < MAX and 0 <= ny < MAX:
+                # (nx, ny)가 아직 방문하지 않은 테두리라면
+
+                if nx == itemX and ny == itemY:
+                    return (move + 1) // 2
+
+                if status[nx][ny] == 1:
+                    q.append((nx, ny, move + 1))
+                    status[nx][ny] = 2
+
+
+'''
+# 1차 풀이
 # 전체의 테두리인지 확인
 def check_line(rect, a, b):
     # 내부
@@ -62,7 +123,7 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
                 visited[nx][ny] = True
 
     return -1
-
+'''
 
 print(solution([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))
 # 17
