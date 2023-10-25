c26aed45a5f180b5f481bd02665ab4901265947d
Jinseo <2000jinseo@gmail.com>
2023-10-25 23:41:50 +0900

    231025 DP (정수 삼각형) #2

--- programmers/43105.py
+++ programmers/43105.py
@@ -1,6 +1,31 @@
 # DP
 # 정수 삼각형
 
+# 2차 풀이
+def solution(triangle):
+    n = len(triangle)
+
+    dp = [[0] * n for _ in range(n)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, n):
+        for j in range(i + 1):
+            # 왼쪽 변
+            if j == 0:
+                dp[i][j] = triangle[i][j] + dp[i - 1][j]
+
+            # 오른쪽 변
+            elif i == j:
+                dp[i][j] = triangle[i][j] + dp[i - 1][j - 1]
+
+            # 내부
+            else:
+                dp[i][j] = triangle[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j])
+
+    return max(dp[-1])
+
+'''
+# 1차 풀이
 def solution(triangle):
     n = len(triangle)
 
@@ -16,3 +41,4 @@ def solution(triangle):
             triangle[x][y] += max(triangle[x - 1][y - 1], triangle[x - 1][y])
 
     return max(triangle[n - 1])
+'''
