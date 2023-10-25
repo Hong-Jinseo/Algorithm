ed24cd730c48cbb4c29fcd7622db64eab93b1161
Jinseo <2000jinseo@gmail.com>
2023-10-26 00:04:12 +0900

    231026 DP (등굣길) #2

--- programmers/42898.py
+++ programmers/42898.py
@@ -1,20 +1,18 @@
 # DP
 # 등굣길
 
+# 2차 풀이
 def solution(m, n, puddles):
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp = [[0] * (m + 2) for _ in range(n + 2)]
     dp[1][0] = 1
-    # 시작점 dp[1][1]을 1로 만들기 위함
-    # (dp[1][1] = dp[1][0] + dp[0][1] = 1 + 0)
 
-    for x in range(1, n + 1):
-        for y in range(1, m + 1):
-            # 만약 (x, y)가 웅덩이면 -> 값을 0으로
-            if [y, x] in puddles:
-                dp[x][y] = 0
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            # 웅덩이라면
+            if [j, i] in puddles:  # 주의!! 행, 열 반대로 주어짐
+                dp[i][j] = 0
                 continue
 
-            # (x, y)에 가는 경우의 수 = 위 + 왼쪽
-            dp[x][y] = (dp[x - 1][y] + dp[x][y - 1]) % 1000000007
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
 
-    return dp[-1][-1]
+    return dp[n][m]
