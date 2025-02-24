183fc856f567b72352d09953bd598fac9c35bf88
Jinseo <2000jinseo@gmail.com>
2025-02-24 20:19:11 +0900

    250224 해시 (의상)

--- programmers/42578.py
+++ programmers/42578.py
@@ -3,18 +3,28 @@
 
 from collections import Counter
 
-
 def solution(clothes):
-    answer = 1
-    cnt = Counter()
-
-    for item in clothes:
-        cnt[item[1]] += 1
-
-    for c in cnt:
-        answer *= (cnt[c] + 1)
-
-    return answer - 1
+    result = 1
+    category = dict()
+    
+    # 카테고리 별 의상 개수 세기
+    for n, c in clothes:
+        if c in category:
+            category[c] += 1
+        else:
+            category[c] = 1
+    
+    # 각 의상을 고르는 경우 + 아예 안 고르는 경우
+    for key in category:
+        result *= (category[key] + 1)
+    
+    # 모든 의상을 안 고르는 경우 제외
+    return result - 1
 
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
+
+'''
+[["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+[["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]	3
+'''
