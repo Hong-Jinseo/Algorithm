0fd8b8a53e11f7890b45cec0eb1ab63462ade021
Jinseo <2000jinseo@gmail.com>
2025-02-24 20:57:20 +0900

    250224 큐 (같은 숫자는 싫어)

--- programmers/12906.py
+++ programmers/12906.py
@@ -2,14 +2,20 @@
 # 같은 숫자는 싫어
 
 def solution(arr):
-    answer = []
+    prev = arr[0]
+    answer = [prev]
 
-    pre = arr[0]
-    answer.append(pre)
-    for now in arr[1:]:
-        if pre != now:
-            answer.append(now)
-            pre = now
+    # 원소가 하나일 경우
+    if len(arr) == 1:
+        return arr
+
+    for num in arr[1:]:
+        if num == prev:
+            # 해당 if문 생략하고 !=만 써도 됨
+            continue
+        else:
+            answer.append(num)
+            prev = num
 
     return answer
 
