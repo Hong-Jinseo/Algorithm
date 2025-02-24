3db29815988ff3ed6d7a75d5b7170576c96292a0
Jinseo <2000jinseo@gmail.com>
2025-02-24 19:44:23 +0900

    250224 해시 (완주하지 못한 선수)

--- programmers/42576.py
+++ programmers/42576.py
@@ -8,8 +8,6 @@ def solution(participant, completion):
     for i in range(len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
-
     return participant[-1]
 
-
-print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
