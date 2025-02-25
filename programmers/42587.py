dfa562c0d4ae12937afd70660ae697c4eb14129b
Jinseo <2000jinseo@gmail.com>
2025-02-25 21:31:03 +0900

    250225 스택/큐 (프로세스)

--- programmers/42587.py
+++ programmers/42587.py
@@ -3,29 +3,35 @@
 
 from collections import deque
 
-
 def solution(priorities, location):
-    count = 0
-
-    priority = []
-    for i in range(len(priorities)):
-        priority.append((priorities[i], i))
-    q = deque(priority)
+    q = deque(priorities)
+    answer = 0
 
     while q:
-        now = q.popleft()
+        target = q.popleft()
+        location -= 1   # 타겟 프로세스 위치
 
-        # if now보다 우선순위가 높은 프로세스가 큐에 있다면 -> 다시 큐에 넣음
-        if any(now[0] < pri[0] for pri in q):
-            q.append(now)
+        if not q:
+            return answer + 1   # 이 경우도 pop되는 케이스이니 +1
+        
+        elif target < max(q):
+            q.append(target)    # 다시 큐에 추가되는 경우, answer+=1 X
+            if location == -1:
+                location = len(q) - 1
 
-        # else 프로세스 실행
         else:
-            count += 1
-            # 실행한 프로세스가 타겟 프로세스라면
-            if now[1] == location:
-                return count
-
+            answer += 1         # pop 될 경우에만 +1 해줌
+            if location == -1:
+                return answer
+            
+    return answer
+    
 
 print(solution([1, 1, 9, 1, 1, 1], 0))
 # 5
+
+'''
+priorities	location	return
+[2, 1, 3, 2]	2	1
+[1, 1, 9, 1, 1, 1]	0	5
+'''
