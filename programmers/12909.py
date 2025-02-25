4d12e36d1aeeeae177c0fe926f514196c7b43bf6
Jinseo <2000jinseo@gmail.com>
2025-02-25 21:00:50 +0900

    250225 스택/큐 (올바른 괄호)

--- programmers/12909.py
+++ programmers/12909.py
@@ -2,19 +2,37 @@
 # 올바른 괄호
 
 def solution(s):
-    stack = []
+    stack = list()
 
-    for now in s:
-        # 열린 괄호일 때
-        if now == "(":
-            stack.append(now)  # 스택에 저장
-        # 닫힌 괄호일 때
-        else:
-            # 스택이 비었거나, 스택에서 닫힌 괄호가 나온다면
-            if not stack or stack.pop() == now:
+    for brace in s:
+        if brace == '(':
+            stack.append(brace)
+        elif stack :
+            # input이 )인 경우
+            top = stack.pop()
+            if top != '(':
                 return False
-
+        else:
+            # input이 )인데 stack이 비어있는 경우
+            return False
+        
+        ''' # 조건문 개선
+        if brace == '(':
+            stack.append(brace)
+        elif (not stack or stack.pop() == brace):
+            return False
+        '''
+    
     # 스택에 괄호가 남아있다면
     if stack:
         return False
     return True
+
+print(solution(")()("))
+
+'''
+"()()"	true
+"(())()"	true
+")()("	false
+"(()("	false
+'''
