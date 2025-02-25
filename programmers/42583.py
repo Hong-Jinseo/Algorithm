909ec34b7f028268523d3244a46ef459c7036b47
Jinseo <2000jinseo@gmail.com>
2025-02-25 22:35:49 +0900

    250225 스택/큐 (다리를 지나는 트럭)

--- programmers/42583.py
+++ programmers/42583.py
@@ -3,29 +3,29 @@
 
 from collections import deque
 
-
 def solution(bridge_length, weight, truck_weights):
+    bridge = deque([0 for _ in range(bridge_length)])
+    bridge_sum = 0  # sum(bridge) 사용할 경우 시간초과 발생하여 bridge_sum 변수 사용
     truck_weights = deque(truck_weights)
-    bridge = deque([0] * bridge_length)
-    current_weight = 0
-    second = 0
+    answer = 0
 
+    # 대기 트럭이 있으면
     while truck_weights:
+        answer += 1
+        bridge_sum -= bridge.popleft()
 
-        passed = bridge.popleft()
-        current_weight -= passed
-
-        if current_weight + truck_weights[0] <= weight:
-            truck = truck_weights.popleft()
-            bridge.append(truck)
-            current_weight += truck
+        # 현재 하중 + 신규 트럭 <= 최대 무게
+        if bridge_sum + truck_weights[0] <= weight:
+            new = truck_weights.popleft()
+            bridge.append(new)
+            bridge_sum += new
         else:
             bridge.append(0)
+    
+    # 다리 위에 남아있는 트럭
+    answer += len(bridge)
+    
+    return answer
 
-        second += 1
-
-    return second + bridge_length
-
-
-print(solution(2, 10, [7, 4, 5, 6]))
-# 8
+print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
+# 110
